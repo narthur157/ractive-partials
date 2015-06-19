720b448a5a67a0b4b6db92df0ea5547078105591
Nicholas Arthur <narthur@adobe.com>
2015-06-19 10:38:33 -0400

    use requirejs

--- dist/rapr.js
+++ dist/rapr.js
@@ -1,57 +1,57 @@
-'use strict';
+define(['exports', 'ractive'], function (exports, _ractive) {
+  'use strict';
 
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-exports.load = load;
+  Object.defineProperty(exports, '__esModule', {
+    value: true
+  });
+  exports.load = load;
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-var _ractive = require('ractive');
+  var _Ractive = _interopRequireDefault(_ractive);
 
-var _ractive2 = _interopRequireDefault(_ractive);
+  // Finds all '{{> partialName }}' in the template
+  var findPartial = /{{>\s?([^\s]+)\s?}}/gi;
 
-// Finds all '{{> partialName }}' in the template
-var findPartial = /{{>\s?([^\s]+)\s?}}/gi;
+  function load(moduleName, require, done) {
+    require('text!' + moduleName, function (text) {
 
-function load(moduleName, require, done) {
-  require('text!' + moduleName, function (text) {
+      var toGet = [];
 
-    var toGet = [];
+      var repartial = text.replace(findPartial, function (match, partial) {
+        // replace slash with $
+        var safePartialKey = partial.replace(/\//g, '$');
 
-    var repartial = text.replace(findPartial, function (match, partial) {
-      // replace slash with $
-      var safePartialKey = partial.replace(/\//g, '$');
-
-      // remember to grab partial
-      if (~partial.indexOf('/')) {
-        toGet.push({
-          safeKey: safePartialKey,
-          path: partial
-        });
-      }
-
-      return '{{> ' + safePartialKey + ' }}';
-    });
-
-    var compiled = _ractive2['default'].parse(repartial);
-
-    if (toGet.length) {
-      require(toGet.map(function (partial) {
-        return partial.path;
-      }), function () {
-        for (var _len = arguments.length, parsed = Array(_len), _key = 0; _key < _len; _key++) {
-          parsed[_key] = arguments[_key];
+        // remember to grab partial
+        if (~partial.indexOf('/')) {
+          toGet.push({
+            safeKey: safePartialKey,
+            path: partial
+          });
         }
 
-        toGet.forEach(function (partial, i) {
-          return _ractive2['default'].partials[partial.safeKey] = parsed[i];
-        });
-        done(compiled);
+        return '{{> ' + safePartialKey + ' }}';
       });
-    } else {
-      done(compiled);
-    }
-  });
-}
+
+      var compiled = _Ractive['default'].parse(repartial);
+
+      if (toGet.length) {
+        require(toGet.map(function (partial) {
+          return partial.path;
+        }), function () {
+          for (var _len = arguments.length, parsed = Array(_len), _key = 0; _key < _len; _key++) {
+            parsed[_key] = arguments[_key];
+          }
+
+          toGet.forEach(function (partial, i) {
+            return _Ractive['default'].partials[partial.safeKey] = parsed[i];
+          });
+          done(compiled);
+        });
+      } else {
+        done(compiled);
+      }
+    });
+  }
+});
 
